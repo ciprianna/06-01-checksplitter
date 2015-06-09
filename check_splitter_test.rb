f3c67b37621acba572e356a69933ce072706f611
Ciprianna <ciprianna17@gmail.com>
2015-06-09 13:04:47 -0500

    Added in more test methods to check_splitter_test.rb - Test the method neg_tip for if a negative tip amount was entered. - Tests the method for people_count for if less than 1 person was entered.

--- check_splitter_test.rb
+++ check_splitter_test.rb
@@ -8,7 +8,6 @@ class CheckSplitterTest < Minitest::Test
   #   with default values. I will test if it returns that hash as expected.
   def test_initialize_hash_created
     new_check = CheckSplitter.new
-    new_check.args
 
     assert_equal({total_cost: 20, tip_percentage: 0.15, people: 2},new_check.args)
   end
@@ -17,20 +16,30 @@ class CheckSplitterTest < Minitest::Test
   #   take an Integer or Float, and return it as a Float.
   def test_tip_percentage
     new_check = CheckSplitter.new(tip_percentage: 15)
-    new_check.tip_percent(new_check.args)
 
     assert_equal(0.15, new_check.tip_percent(new_check.args))
   end
 
   # Another test will be for the neg_tip method. It should be able to take a
   #   negative Float as an input and set the value to 0.15.
+  def test_negative_tip_amounts
+    new_check = CheckSplitter.new(tip_percentage: -15)
+
+    assert_equal(0.15, new_check.neg_tip(new_check.args))
+  end
 
   # Another test is for the people_count method. This will set the Integer value
   #   of people to one if the value is input <= 0.
+  def test_people_count
+    new_check = CheckSplitter.new(people: 0)
+
+    assert_equal(1, new_check.people_count(new_check.args))
+  end
 
   # A test for the total_cost_of_meal method should take two values as Integers
   #   and return the first value (total_cost) plus the first value times the
   #   second value (tip_percentage)
+  
 
   # The base_cost_per_person method should return the first value input, Float,
   #   divided by the second value input, an Integer.
