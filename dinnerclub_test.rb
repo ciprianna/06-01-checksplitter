2ec59db8fa98a7a370313e3243f488fef22874b6
Ciprianna <ciprianna17@gmail.com>
2015-06-10 15:54:50 -0500

    Created the final test for DinnerClub.   Tested the running_balance_update method.
    All tests successful.

--- dinnerclub_test.rb
+++ dinnerclub_test.rb
@@ -17,12 +17,20 @@ class DinnerClubTest < Minitest::Test
   #   first Hash when two Hashes are given by destructively merging them.
   def test_history_update
     dinner = DinnerClub.new
-    dinner.new_event(10, 10, ["Calypso"], "06-10-2015", "Dundee Dell", "everyone")
+    club_history = {}
+    one_event = {"06-10-2015" => ["Dundee Dell", {"Calypso" => 11}]}
 
-    assert_equal({"06-10-2015" => ["Dundee Dell", {"Calypso" => 11}]}, dinner.club_history)
+    assert_equal({"06-10-2015" => ["Dundee Dell", {"Calypso" => 11}]}, dinner.club_history_update(club_history, one_event))
   end
 
   # Test for the running_balance_update method. Should show that the Hash has
   #   been modified.
+  def test_running_balance_update
+    dinner = DinnerClub.new
+    club_history_ex = {"06-10-2015" => ["Dundee Dell", {"Calypso" => 11}]}
+    dinner.running_balance_update(dinner.running_balance, club_history_ex)
+
+    assert_equal({"Calypso" => 11}, dinner.running_balance)
+  end
 
 end
